caf152eb114d3eb9cc32ee634fb8714cb4a8ced9
Alvin <huerox@msn.com>
2020-09-10 18:27:12 -0700

    completed sign in page.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Checkout from './Checkout';
+import Login from './Login';
 
 
 function App() {
   return (
     <Router>
       <div className="app">
-      <Header />
-
         <Switch>
+        <Route path='/login'>
+           <Login />
+        </Route>
         <Route path='/checkout'>
-            <Checkout />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
+          <Header />
+          <Checkout />
+        </Route>
+        <Route path='/'>
+          <Header />
+          <Home />
+        </Route>
         </Switch>
       </div>
     </Router>
