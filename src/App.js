7b5a4c4f46629e0c1441374d71f09bce75f149bc
Alvin <huerox@msn.com>
2020-09-10 22:42:19 -0700

    added user authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Checkout from './Checkout';
 import Login from './Login';
-
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, []) //will only run once when app component loads if array is empty
+
   return (
     <Router>
       <div className="app">
